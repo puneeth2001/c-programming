be501d9d83bcbaa12e9786f66acae6b7c329edf4
puneethchanda <puneethchanda2001@gmail.com>
2020-07-20 13:28:29 +0530

    initial commit

--- DNode.java
+++
@@ -1,5 +0,0 @@
-public class DNode{
-    int data;
-    DNode next;
-    DNode prev;
-}
--- Problem Solving/police.cpp
+++ Problem Solving/police.cpp
@@ -1,22 +1,20 @@
-#include <iostream>
+# include<iostream>
 using namespace std;
 int main(){
-    int i,n,sum=0,dong=0;
+    int n;
+    int pol=0,the=0;
     cin >> n;
     int arr[n];
-    for(i=0;i<n;i++){
-        cin>>arr[i];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
     }
-    for(i=0;i<n;i++){
+    for(int i=0;i<n;i++){
+        if(arr[i]<0){
+            the++;
+        }
         if(arr[i]>0){
-            sum=sum+arr[i];
-        }
-        else if(arr[i]<0 && sum>0){
-            sum=sum-1;
-        }
-        else if(arr[i]<0 && sum ==0){
-            dong = dong +1;
+            i=i+arr[i];
         }
     }
-    cout << dong;
+    cout << the;
 }
